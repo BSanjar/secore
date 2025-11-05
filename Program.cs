18a2c22408df6366c4fc45f4f8a897121f1b932b
sanjar <s.baigaziev@dpa.gov.kg>
2025-11-05 21:14:43 +0600

    Enable Development mode for debugging - show detailed error messages

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 
-//Подключение к БД
+//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅ
 builder.Services.AddDbContext<AppDbContext>(
     options =>
     {
@@ -17,14 +17,18 @@ builder.Services.AddDbContext<AppDbContext>(
         .UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
     });
 
-//Конфиги
+//пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
